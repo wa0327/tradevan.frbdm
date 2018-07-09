61400afb61d910fa80cc06d307cf56a1054cb67b
Jack Tsai <wa0327@gmail.com>
2018-07-09 13:30:46 +0800

    當登入者是[系統管理員]群組時，顯示管理者選單。

--- src/frbdm.web.portal.frontend/src/app/app.component.ts
+++ src/frbdm.web.portal.frontend/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizationService } from './authorization.service';
+import { WebapiService } from '../webapi/webapi.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    showAdminMenu = false;
+
+    constructor(
+        private api: WebapiService
+    ) {
+    }
+
+    ngOnInit() {
+        this.api.getLogon().subscribe(logon => {
+            if (logon != null) {
+                this.showAdminMenu = logon.roles.some(r => r == '系統管理員');
+            }
+        });
+    }
 }
